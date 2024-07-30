8b44ae4e144a2c980515cb9cca2c0a20beaab92f
AhammedNoyon <mdnoyon956993@gmail.com>
2024-07-31 00:59:30 +0600

    last commit for this html file

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,3 +1,14 @@
-alert("Hello");
-document.write("Hello would,Iam Ahamed Noyon");
-console.log("10");
+
+alert("Hello would");
+// document.write("Hello would,Iam Ahamed Noyon");
+// console.log("10");
+
+
+// task-1
+document.write("Name : Ahammed Noyon <br/>");
+document.write("university : Govt titumir college <br/>");
+document.write("subjects : Management <br/>");
+document.write("address : uttara <br/>");
+document.write("phone : 01888475550 <br/>");
+document.write("skills : Jonior web developer <br/>");
+document.write("Name : Ahammed Noyon");
