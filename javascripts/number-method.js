f4765fca80acb945a99d606c004682353fedd0ef
AhammedNoyon <mdnoyon956993@gmail.com>
2024-07-31 02:24:35 +0600

    first commit for numbering method file

---
+++ javascripts/number-method.js
@@ -0,0 +1,36 @@
+
+        //string theke number a conbert
+
+// var num=20;
+// num=toString(num);
+// console.log(typeof(num));
+
+         //string theke 
+
+// var num="20";
+// num=parseInt(num);
+// console.log(typeof(num));
+
+
+        //string k doshomic shongkhai conbert
+
+
+//  var num="2.566";
+// num=parseFloat(num);
+// console.log(typeof(num));
+
+
+        //use ToFixed and ToPrecision
+
+
+// var num=2.5678;
+
+// console.log(num.toFixed(2));
+// console.log(num.toPrecision(2));
+
+        //boolean theke number 
+
+
+// console.log(Number("13"))
+// console.log(Number(true))
+console.log(Number(false))
